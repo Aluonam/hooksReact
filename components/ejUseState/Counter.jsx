0342ac8ea8d69b60feabbb0482b042aaea95e60b
Paula Cano <paulacanoroman@gmail.com>
2023-11-23 17:16:35 +0100

    feat: counter logic with useState

--- components/ejUseState/Counter.jsx
+++ components/ejUseState/Counter.jsx
@@ -5,8 +5,9 @@ const Counter = () => {
     const [counter, setCounter] = useState(0)
   return (
     <>
-    <button onClick={()=>{}}>+</button>
-    <button onClick={()=>{}}>-</button>
+    {counter}
+    <button onClick={()=>{setCounter(counter + 1)}}>+</button>
+    <button onClick={()=>{setCounter(counter - 1)}}>-</button>
     </>
   )
 }
