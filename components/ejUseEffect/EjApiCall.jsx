06019f801707a5fc93d9bbc182b2dfeba7365ba7
Paula Cano <paulacanoroman@gmail.com>
2023-11-23 18:42:35 +0100

    feat: apiCall with useEffect
    the call only occurs when you refresh de page because useEffect is executed when the component is loaded

--- components/ejUseEffect/EjApiCall.jsx
+++ components/ejUseEffect/EjApiCall.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const EjApiCall = () => {
 
+    const [dataAPI, setDataAPI] = useState([])
+
+    useEffect(() => {
+
+        const fetchData = async ()=>{
+            try{
+                const dataFetch = await fetch(`https://v2.jokeapi.dev/joke/Programming?lang=es`)
+                const data = await dataFetch.json()
+                setDataAPI(data)
+            } catch(error){error, "error detected"}
+        }
+        fetchData()
+    }, [])
     
   return (
-    <div>ejApiCall</div>
+    <div>
+        {dataAPI.joke}
+    </div>
   )
 }
 
