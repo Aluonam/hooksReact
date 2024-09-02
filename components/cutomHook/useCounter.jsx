9d4cd6bd7a2e2d5192037eb0216e7f0f13ed8122
Paula Cano <paulacanoroman@gmail.com>
2024-09-02 12:35:26 +0200

    feaT: useCounter state and const

--- components/cutomHook/useCounter.jsx
+++ components/cutomHook/useCounter.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 
 const useCounter = () => {
+ const [counter, setCounter] = useState(0);
 
+ const increase = ()=>setCounter(counter+1)
+ const decrement = ()=>setCounter(counter-1)
+ const reset = ()=>setCounter(0)
   
 }
 
