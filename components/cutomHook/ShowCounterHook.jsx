4fd0b9daba68030a97965e1ab061cb0d873108b0
Paula Cano <paulacanoroman@gmail.com>
2024-09-02 13:15:20 +0200

    style: ShowCounter with low style

--- components/cutomHook/ShowCounterHook.jsx
+++ components/cutomHook/ShowCounterHook.jsx
@@ -5,7 +5,7 @@ const ShowCounterHook = () => {
 
     const {counter, increase, decrement, reset}  = useCounter()
   return (
-    <div >
+    <div style={{width:'300px', height:'150px', border: '1px solid blue', backgroundColor:'aliceblue', display:'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
         <h3>Counter with customHook:</h3>
         <div>{counter}</div>
         <button onClick={increase}>+</button>
