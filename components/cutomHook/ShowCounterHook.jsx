d57fe928ecd563062832ccdbbae85dac7b9ba31b
Paula Cano <paulacanoroman@gmail.com>
2024-09-02 14:39:01 +0200

    feat: show and apply useDoubleCounter

--- components/cutomHook/ShowCounterHook.jsx
+++ components/cutomHook/ShowCounterHook.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import useCounter from './useCounter'
+import useDoubleCounter from './useDoubleCounter'
 
 const ShowCounterHook = () => {
 
     const {counter, increase, decrement, reset}  = useCounter()
+
+    const counterA = useDoubleCounter()
+    const counterB = useDoubleCounter()
+
   return (
     <>
     <div style={{width:'300px', height:'150px', border: '1px solid blue', backgroundColor:'aliceblue', display:'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
@@ -13,8 +18,9 @@ const ShowCounterHook = () => {
         <button onClick={decrement}>-</button>
         <button onClick={reset}>reset</button>
     </div> 
-    <div>
-        
+    <div style={{width:'300px', height:'150px', border: '1px solid mediumseagreen', backgroundColor:'honeydew', display:'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
+        <div>{counterA.counterDouble}</div>
+        <div>{counterB.counterDouble}</div>
     </div>
     </>
   )
