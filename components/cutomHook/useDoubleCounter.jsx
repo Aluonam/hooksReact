50db3bec917e0337bf0130c64a055487f20485a2
Paula Cano <paulacanoroman@gmail.com>
2024-09-02 14:38:15 +0200

    feat: customhook doubleCounter

--- components/cutomHook/useDoubleCounter.jsx
+++ components/cutomHook/useDoubleCounter.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react'
 
 const useDoubleCounter = ()=>{
+    const [counterDouble, setCounterDouble] = useState(0);
+
+    const increaseDouble = ()=>setCounterDouble(counterDouble+1)
+    const decrementDouble = ()=>setCounterDouble(counterDouble-1)
+    const resetDouble = ()=>setCounterDouble(0)
 
     return{
-        
+        counterDouble,
+        increaseDouble,
+        decrementDouble,
+        resetDouble
     }
 }
+
+export default useDoubleCounter
